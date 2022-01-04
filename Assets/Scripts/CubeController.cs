789220f2fedb4a3a00b95db22bb3517031e1c535
Faruq Afolabi <gbolayo13@gmail.com>
2022-01-03 21:25:39 -0500

    Part rotation
    - Got a bit of part rotation working

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class CubeController : MonoBehaviour
 {
-
     [SerializeField] private GameObject[,,] _cubeMatrix = new GameObject[3, 3, 3];
     [Range(0,5)][SerializeField] private float _speed;
 
@@ -15,12 +14,17 @@ public class CubeController : MonoBehaviour
     private Rigidbody _rb;
     private Rigidbody _pRb;
     private GameObject _partSelect;
+    private int _cubeLayerMask;
+    private int _cubePartLayerMask;
 
     // Start is called before the first frame update
     void Start()
     {
+        _cubeLayerMask = 1 << 6;
+        _cubePartLayerMask = 1 << 7;
         _rb = GetComponent<Rigidbody>();
         _partSelect = new GameObject("PartSelect");
+        _partSelect.transform.parent = transform.parent;
         _pRb = _partSelect.AddComponent<Rigidbody>();
         _pRb.useGravity = false;
         for (int i = 0; i < transform.childCount; i++)
@@ -33,6 +37,7 @@ public class CubeController : MonoBehaviour
     void Update()
     {
         RotateCube();
+        RotatePart();
         PartSelect();
 
         if (Input.GetMouseButton(0) || Input.GetMouseButton(1))
@@ -58,26 +63,6 @@ public class CubeController : MonoBehaviour
             if (_yawMouse != 0) _rb.AddTorque(-transform.parent.transform.up * _speed * _yawMouse);
 
             if (_pitchMouse != 0) _rb.AddTorque(-transform.parent.transform.right * _speed * _pitchMouse);
-
-            /*
-            Vector3 rotation = Vector3.zero;
-
-            if (yawMouse > 0) rotation += -transform.parent.transform.up;
-            else if (yawMouse < 0) rotation += transform.parent.transform.up;
-
-            if (pitchMouse > 0) rotation += -transform.parent.transform.right;
-            else if (pitchMouse < 0) rotation += transform.parent.transform.right;
-
-            transform.Rotate(rotation, _speed);
-            */
-
-            /*
-            float newX = pitchMouse * Mathf.Cos(Mathf.Deg2Rad * transform.eulerAngles.z) + yawMouse * Mathf.Sin(Mathf.Deg2Rad * transform.eulerAngles.y);
-            float newY = yawMouse * Mathf.Cos(Mathf.Deg2Rad * transform.eulerAngles.z) + pitchMouse * Mathf.Sin(Mathf.Deg2Rad * transform.eulerAngles.x);
-            float newZ = pitchMouse * Mathf.Cos(Mathf.Deg2Rad * transform.eulerAngles.x) + yawMouse * Mathf.Sin(Mathf.Deg2Rad * transform.eulerAngles.y);
-
-            transform.Rotate(new Vector3(-newX * 2, -newY * 2, 0));
-            */
         }
         else
         {
@@ -87,17 +72,42 @@ public class CubeController : MonoBehaviour
         }
     }
 
+    private void RotatePart()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            _pRb.isKinematic = false;
+
+            if (Mathf.Abs(_yawMouse) > Mathf.Abs(_pitchMouse) && !_lockH && !_lockV) _lockV = true;
+            if (Mathf.Abs(_pitchMouse) > Mathf.Abs(_yawMouse) && !_lockH && !_lockV) _lockH = true;
+
+            if (!_lockH && _lockV) _pRb.AddTorque(-transform.up * _speed * _yawMouse);
+
+            if (!_lockV && _lockH) _pRb.AddTorque(-transform.right * _speed * _pitchMouse);
+        }
+        else
+        {
+            _pRb.isKinematic = true;
+            _lockH = false;
+            _lockV = false;
+        }
+    }
+
     private void PartSelect()
     {
         if (Input.GetMouseButton(1))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out RaycastHit raycastHit, 100f))
+            if (Physics.Raycast(ray, out RaycastHit raycastHit, 100f, _cubePartLayerMask))
             {
+                Debug.Log("Ray");
                 if (raycastHit.transform != null)
                 {
                     GameObject g = raycastHit.transform.gameObject;
-                    RotateParts(g);
+
+                    if (!_lockH && _lockV) AddPartsH(g);
+
+                    if (!_lockV && _lockH) AddPartsV(g);
                 }
             }
         }
@@ -105,67 +115,42 @@ public class CubeController : MonoBehaviour
         {
             if (_partSelect.transform.childCount != 0)
                 for(int i = 0; i < _partSelect.transform.childCount; i++)
-                    _partSelect.transform.GetChild(i).parent = transform.parent;
-
-            _pRb.isKinematic = true;
-            _lockH = false;
-            _lockV = false;
+                    _partSelect.transform.GetChild(i).parent = transform;
         }
     }
 
-    private void RotateParts(GameObject g)
-    {
-        if (_yawMouse != 0 && !_lockH && !_lockV) _lockH = true;
-        if (_pitchMouse != 0 && !_lockH && !_lockV) _lockV = true;
-
-        if (_lockV) RotatePartsH(g);
-        //if (_lockH) RotatePartsV(g);
-    }
-
-    private void RotatePartsH(GameObject o)
-    {
-        GameObject[] parts = GetPartsH(o);
-
-        if(_partSelect.transform.childCount == 0)
-            foreach (GameObject g in parts) g.transform.parent = _partSelect.transform;
-
-        _pRb.isKinematic = false;
-        _pRb.AddTorque(-transform.up * _speed * _yawMouse);
-
-        //foreach (GameObject g in parts) g.transform.Rotate(transform.up, _speed);
-    }
-
-    private void RotatePartsV(GameObject o)
-    {}
-
-    private void RotatePartsV1(GameObject o)
-    { }
-
-    private void RotatePartsV2(GameObject o)
-    { }
-
-    private GameObject[] GetPartsH(GameObject o)
+    private void AddPartsH(GameObject o)
     {
         int y = GetY(o.name);
         GameObject[] output = new GameObject[9];
 
-        int i = 0;
         for (int x = 0; x < 3; x++)
             for (int z = 0; z < 3; z++)
             {
-                output[i] = _cubeMatrix[x, y, z];
-                Debug.Log(_cubeMatrix[x, y, z]);
+                _cubeMatrix[x, y, z].transform.parent = _partSelect.transform;
+                Debug.Log("Part: " + _cubeMatrix[x, y, z].name);
             }
-
-        return output;
     }
 
-    private GameObject[] GetPartsV1(GameObject o)
+    private void AddPartsV(GameObject o)
     {
-        return null;
+        int x = GetX(o.name);
+        GameObject[] output = new GameObject[9];
+        
+        for (int y = 0; y < 3; y++)
+            for (int z = 0; z < 3; z++)
+            {
+                _cubeMatrix[x, y, z].transform.parent = _partSelect.transform;
+                Debug.Log("Part: " + _cubeMatrix[x, y, z].name);
+            }
     }
 
-    private GameObject[] GetPartsV2(GameObject o)
+    private void UpdateOrientation()
+    {
+
+    }
+
+    private GameObject[] AddPartsV2(GameObject o)
     {
         return null;
     }
